287e59cc8f77bcfe90f8a7f7ee28420b9d1dac56
Yusuke Matsubara <whym@whym.org>
2012-11-29 20:21:54 +0900

    * ja test

--- src/test/scala/TestMultipleSequenceAlignment.scala
+++ src/test/scala/TestMultipleSequenceAlignment.scala
@@ -136,4 +136,12 @@ class TestMultipleSequenceAlignment extends FunSuite {
       linear_dag("$b$").align(linear_dag("$zbxx$"), weight())
     }
   }
+
+  test("JA") {
+    expect(Dag("$色いろは匂にえほどへ散とちりぬるを$".toCharArray.toList,
+               Set((0,1), (1,4), (4,5), (5,7), (7,9), (9, 11), (11,14), (14,15), (15,16), (16,17), (17,18),
+                   (0,2), (2,3), (3,4), (4, 6), (6,8), (8,10), (10,12), (12,13), (13,14)))) {
+      linear_dag("$色は匂えど散りぬるを$").align(linear_dag("$いろはにほへとちりぬるを$"), weight())
+    }
+  }
 }
