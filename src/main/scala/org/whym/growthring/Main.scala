0ec5ee1b24f534356984792946f07355316a678c
Yusuke Matsubara <whym@whym.org>
2013-01-17 20:24:59 +0900

    * option for repeats

--- src/main/scala/org/whym/growthring/Main.scala
+++ src/main/scala/org/whym/growthring/Main.scala
@@ -18,24 +18,28 @@ object Main {
     import scala.io
     import scala.util.Properties
     val strings = args.map(io.Source.fromFile(_).getLines.toList).flatMap(x => x).toList
-    val es = new ExtremalSubstrings(strings.mkString("\n"))
-    for ( x <- es.maxRepeats(Properties.propOrElse("maxRepeats", "3").toInt) ) {
-      System.err.println("r " + new String(strings.mkString("\n").slice(x._1, x._2 + 1))) //!
-    }
-    for ( x <- es.minUniques ) {
-      System.err.println("u " + new String(strings.mkString("\n").slice(x._1, x._2 + 1))) //!
-    }
-    System.err.println("--") //!
-    System.err.flush //!
-    val msa = new MultipleSequenceAlignment[Char](strings.map(x => ("^"+x+"$").toCharArray.toIndexedSeq))
-    val dag = msa.align.compact((x,y) => x.concat(y))
-    def nodeformat(i: Int, x: MultipleSequenceAlignment.Node[Char]): String = {
-      def escape(x: String) = x.replace("\\","\\\\").replace("\"", "\\\"")
-      "  N_%d[label=\"%s\",fontsize=%f];".format(i, escape(x.label.map(_.toString).mkString),
-                                       10 + scala.math.log(x.freq) * 4)
-    }
-    for ( line <- dag.dot(nodeformat) ) {
-      println(line)
+    Properties.propOrElse("mode", "repeats") match {
+      case "repeats" => {
+        val es = new ExtremalSubstrings(strings.mkString("\n"))
+        for ( x <- es.maxRepeats(Properties.propOrElse("maxRepeats", "3").toInt) ) {
+          println("r " + new String(strings.mkString("\n").slice(x._1, x._2 + 1))) //!
+        }
+          for ( x <- es.minUniques ) {
+            println("u " + new String(strings.mkString("\n").slice(x._1, x._2 + 1))) //!
+          }
+      }
+      case _ => {
+        val msa = new MultipleSequenceAlignment[Char](strings.map(x => ("^"+x+"$").toCharArray.toIndexedSeq))
+        val dag = msa.align.compact((x,y) => x.concat(y))
+        def nodeformat(i: Int, x: MultipleSequenceAlignment.Node[Char]): String = {
+            def escape(x: String) = x.replace("\\","\\\\").replace("\"", "\\\"")
+          "  N_%d[label=\"%s\",fontsize=%f];".format(i, escape(x.label.map(_.toString).mkString),
+                                                     10 + scala.math.log(x.freq) * 4)
+          }
+        for ( line <- dag.dot(nodeformat) ) {
+          println(line)
+          }
+      }
     }
   }
 }
