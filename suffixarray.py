0884ec1220fc27458ad46378a3e8ed563b0eee9a
Yusuke Matsubara <whym@whym.org>
2014-03-04 00:09:37 +0900

    Fix suffix sort (delimited version and normal version)

--- suffixarray.py
+++ suffixarray.py
@@ -1,11 +1,33 @@
 from itertools import *
 
-DELIMIT = '$'
+DELIMIT = '#'
 
 def suffixes(s):
-    return sorted(xrange(0, len(s)), key=lambda i: s[i])
+    '''sorted suffixes'''
+    return sorted(xrange(0, len(s)), key=lambda i: s[i:])
+
+def merge(left, right, key):
+    '''merge sort module'''
+    if not len(left) or not len(right):
+        return left or right
+
+    result = []
+    i, j = 0, 0
+    while (len(result) < len(left) + len(right)):
+        if key(left[i]) < key(right[j]):
+            result.append(left[i])
+            i+= 1
+        else:
+            result.append(right[j])
+            j+= 1
+        if i == len(left) or j == len(right):
+            result.extend(left[i:] or right[j:])
+            break 
+
+    return result
 
 def suffixes_delimited(s):
+    '''suffixes sorted by the delimiting method'''
     # split
     a = [x + DELIMIT for x in s.split(DELIMIT)]
     if a[-1] == DELIMIT:
@@ -19,16 +41,21 @@ def suffixes_delimited(s):
 
     # merge suffix arrays for splits
     ret = []
-    for x in [[lens[i] + j] for j in suffixes(x) for (i,x) in enumerate(a)]:
-        ret += x
+    for (i,x) in enumerate(a):
+        ret = merge(ret, [lens[i] + j for j in suffixes(x)], lambda i: s[i:])
     return ret
 
 if __name__ == '__main__':
     while True:
         s = raw_input('> ').strip() + DELIMIT
-        for i in suffixes_delimited(s):
-            print '%10d %s' % (i, s[i:])
-        print '-----'
-        for i in suffixes(s):
-            print '%10d %s' % (i, s[i:])
+        if suffixes_delimited(s) == suffixes(s):
+            for i in suffixes_delimited(s):
+                print '%10d %s' % (i, s[i:])
+        else:
+            print 'TEST FAILED'
+            for i in suffixes_delimited(s):
+                print '%10d %s' % (i, s[i:])
+            print '-----'
+            for i in suffixes(s):
+                print '%10d %s' % (i, s[i:])
 
