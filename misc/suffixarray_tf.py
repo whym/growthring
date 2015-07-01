4bc0748bdcbab4dfc09d9dfc1666cdf15dc62a2b
Yusuke Matsubara <whym@whym.org>
2015-07-01 16:23:24 +0900

    Narisawa (2007) implementation

--- misc/suffixarray_tf.py
+++ misc/suffixarray_tf.py
@@ -9,20 +9,49 @@
   organization={Springer}
 }
 
+where at line 11, algorithm 2, page 349,
+
+w[BL] != w[BR]
+
+should read as
+
+w[SA[BL]] != w[SA[BR]]
+
 '''
 from itertools import *
 from IPython import embed
 import argparse
+import codecs
+import sys
+import math
+from collections import namedtuple
 
+EC = namedtuple('EC', ('pos', 'freq', 'minimal'))
+ST = namedtuple('ST', ('left', 'height'))
 DELIMIT = '#'
 
 def suffixes(s):
     '''sorted suffixes, computed naively'''
     return sorted(xrange(0, len(s)), key=lambda i: s[i:])
 
-def all_substrings(s):
-    length = len(s)
-    return (s[i: j] for i in xrange(length) for j in xrange(i + 1, length + 1))
+def all_subspans(a, b):
+    for j in xrange(a, b+1):
+        for i in xrange(a, b+1):
+            if i < j:
+                yield (i, j)
+
+# def pincer(w, pos, m):
+#     '''
+#     This implementation considers subspan relations, as opposed to substring relations which were ggested in Narisawa (2007).
+#      '''
+#     for x in all_subspans(pos[0],pos[1]):
+#         if any(x[0] <= y[0] and x[1] >= y[1] for y in m):
+#             yield w[x[0]:x[1]]
+
+def pincer(w, pos, m):
+    for x in all_subspans(pos[0],pos[1]):
+        if any(w[x[0]:x[1]].find(w[y[0]:y[1]]) >= 0 for y in m):
+            yield w[x[0]:x[1]]
 
 def common_prefix_len(x,y):
     i = 0
@@ -42,29 +71,36 @@ def pair2str(w, pair):
     return 'w[%d:%d] ("%s")' % (x, y, w[x:y+1])
 
 def ec(w, sa, lcp, rank):
-    stack = [(-1, -1)]
-    for i in xrange(0, len(sa)):
+    '''
+    All arrays in the arguments 1-start.
+
+    EC.pos is exclusive; w[pos[0]:pos[1]] is the substring it represents.
+    '''
+    lenw = len(w) - 1
+    stack = [ST(-1, -1)]
+    for i in xrange(1, lenw+1):
         l_new = i - 1
         h_new = lcp[i]
-        left = stack[-1][0]
-        height = stack[-1][1]
+        left = stack[-1].left
+        height = stack[-1].height
         while height > h_new:
             stack.pop()
-            if stack[-1][1] > h_new:
-                parent = stack[-1][1]
+            if stack[-1].height > h_new:
+                parent = stack[-1].height
             else:
                 parent = h_new
             l = left
             r = i - 1
             freq = r - l + 1
             rlen = height
-            bl = l
-            br = r
+            bl = 0
+            br = 0
+            pos = (sa[l], sa[l]+rlen)
             if sa[l] != 1 and sa[r] != 1:
                 bl = rank[sa[l] - 1]
                 br = rank[sa[r] - 1]
-            if (br - bl + 1 != freq) or (w[bl] != w[br]) or (sa[l] == 1) or (sa[r] == 1):
-                x = w[sa[l]: sa[l] + rlen]
+            if (br - bl + 1 != freq) or (w[sa[bl]] != w[sa[br]]) or (sa[l] == 1) or (sa[r] == 1):
+                #print w[pos[0]:pos[1]], bl, br, l, r, br - bl + 1 != freq, w[bl] != w[br], sa[l] == 1, sa[r] == 1
                 size = rlen - parent
                 mlen = rlen - parent
                 ln = rlen
@@ -73,57 +109,86 @@ def ec(w, sa, lcp, rank):
                 fr = rank[sa[r] + 1]
                 bl = l
                 br = r
-                while (ln - 1 > lcp[fl]) and (ln-1 > lcp[fr+1]) and (fr - fl +1 == freq):
+                while (ln - 1 > lcp[fl]) and (ln - 1 > lcp[fr+1]) and (fr - fl +1 == freq):
                     if lcp[fl] >= lcp[fr+1]:
                         parent = lcp[fl]
                     else:
                         parent = lcp[fr + 1]
-                    ln = ln = 1
+                    ln = ln - 1
                     size = size + ln - parent
                     if mlen > ln - parent:
-                        minimal.add((sa[bl], sa[bl] + parent))
+                        minimal.add((sa[bl], sa[bl] + parent + 1))
                     bl = fl
                     br = fr
-                    if (sa[fl] + 1 >= len(w)) or (sa[fr] + 1 >= len(w)):
+                    if (sa[fl] + 1 >= lenw) or (sa[fr] + 1 >= lenw):
                         break
                     fl = rank[sa[fl] + 1]
                     fr = rank[sa[fr] + 1]
                     mlen = ln - parent
-                minimal.add((sa[bl], sa[bl] + parent))
-                print '"%s": freq %d, size %d, %s' % (x, freq, size, minimal)
+                minimal.add((sa[bl], sa[bl] + parent + 1))
+                #print '"%s" (%d,%d): freq %d, size %d, %s' % (w[pos[0]:pos[1]], pos[0], pos[1], freq, size, minimal)
                 #print pair2str(w, (sa[l], sa[l] + rlen - 1)), '--', ', '.join([pair2str(w, y) for y in minimal])
-                print ' --', [x for x in all_substrings(x) if any(x.find(w[y[0]:y[1]+1]) >= 0 for y in minimal)]
+                #print ' --', list(pincer(w, pos, minimal))
+                yield EC(pos, freq, [x for x in minimal])
             l_new = left
-            left = stack[-1][0]
-            height = stack[-1][1]
+            left = stack[-1].left
+            height = stack[-1].height
         if height < h_new:
-            stack.append((l_new, h_new))
-        stack.append((i, len(w) - sa[i]))
+            stack.append(ST(l_new, h_new))
+        stack.append(ST(i, lenw - sa[i]))
 
 def next_input(args):
     for x in args:
         yield x
-    while True:
-        yield raw_input('> ').strip()
+    try:
+        while True:
+            s = raw_input('> ').strip()
+            print ''
+            yield s
+    except EOFError:
+        pass
+
+def equivalence_classes(s):
+    sa = suffixes(s)
+    rank = [-1] * (len(sa) + 2)
+    for (x,y) in enumerate(sa):
+        rank[y] = x
+    lcp = longest_common_prefixes(s, sa) + [0]
+
+    # index to start from 1
+    s = DELIMIT + s
+    sa = [0] + [x + 1 for x in sa]
+    lcp = [-1] + lcp
+    rank = [0] + [x + 1 for x in rank]
+
+    #print sa, rank, lcp
+    return [EC((x.pos[0]-1, x.pos[1]-1), x.freq, set((y[0]-1,y[1]-1) for y in x.minimal)) for x in ec(s, sa, lcp, rank)]
 
 if __name__ == '__main__':
+    sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
+
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', '--verbose', action='store_true', default=False,
                         help='turn on verbose message output')
+    parser.add_argument('-i', '--interactive', action='store_true', default=False,
+                        help='turn on interactive mode')
     parser.add_argument('inputs', nargs='*')
     options = parser.parse_args()
-    for line in next_input(options.inputs):
-        s = line + DELIMIT
-        sa = suffixes(line)
-        rank = [len(s) - 1] * (len(sa) + 2)
-        for (x,y) in enumerate(sa):
-            rank[y] = x
-        lcp = longest_common_prefixes(s, sa) + [-1]
-
-        # index to start from 1
-        s = DELIMIT + s
-        sa = [x + 1 for x in sa]
-        lcp = [-1] + lcp
-
-        print sa, rank, lcp
-        ec(s, sa, lcp, rank)
+    if options.interactive:
+        for line in next_input(options.inputs):
+            s = line + DELIMIT
+            for e in equivalence_classes(s):
+                print e, s[e.pos[0]:e.pos[1]]
+    else:
+        if len(options.inputs) == 0:
+            print >>sys.stderr, 'input file missing'
+            exit(1)
+        lines = []
+        for f in options.inputs:
+            for line in codecs.open(f, encoding='utf-8'):
+                lines.append(line + DELIMIT)
+        s = ''.join(lines)
+        for e in sorted(equivalence_classes(s), key=lambda e: -(e.pos[1]-e.pos[0])*math.log(e.freq)):
+            print e
+            print s[e.pos[0]:e.pos[1]].replace('\n', '\\n')
+        
---
+++ misc/tests/test_suffixarray_tf.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import unittest
+import suffixarray_tf as st
+from StringIO import StringIO
+
+def get_pos(s, q):
+    pos = s.find(q)
+    return (pos, pos+len(q))
+
+def all_substrings(s):
+    return [s[x[0]:x[1]] for x in st.all_subspans(0, len(s))]
+
+class TestSuffixArrayTF(unittest.TestCase):
+    def setUp(self):
+        None
+
+    def test_lcp(self):
+        s = 'ABRACADABRA#'
+        self.assertEqual([-1, 0, 1, 4, 1, 1, 0, 3, 0, 0, 0, 2], st.longest_common_prefixes(s, st.suffixes(s)))
+
+    def test_all_subspans(self):
+        self.assertEqual(sorted([(0, 1),
+                                 (0, 2),
+                                 (0, 3),
+                                 (1, 2),
+                                 (1, 3),
+                                 (2, 3)])
+                         , sorted(list(st.all_subspans(0, 3))))
+
+    def test_pincer0(self):
+        p1 = st.pincer(' ABCDEF', (1,3), [(1,2)])
+        p2 = st.pincer('ABCDEF',  (0,2), [(0,1)])
+        self.assertEqual(list(p1), list(p2))
+
+    def test_all_subspans0(self):
+        p1 = st.all_subspans(1,3)
+        p2 = st.all_subspans(0,2)
+        self.assertEqual(sorted(list(p1)), sorted([(x[0]+1, x[1]+1) for x in p2]))
+
+    def test_pincer(self):
+        s = 'ABCDE'
+        q = 'BCD'
+        m = 'C'
+        self.assertEqual(['BC', 'C', 'BCD', 'CD'], list(st.pincer(s, get_pos(s, q), [get_pos(s, m)])))
+
+    def test_tf(self):
+        s = 'mississippi' + st.DELIMIT
+        ec = st.equivalence_classes(s)
+        self.assertEqual(['issi', 'i', 'mississippi', 'p'], [s[x.pos[0]:x.pos[1]] for x in ec if x])
+
+    def test_equiv(self):
+        s = 'abrab' + st.DELIMIT
+        ec = st.equivalence_classes(s)
+        ss = set()
+        for x in ec:
+            for y in list(st.pincer(s, x.pos, x.minimal)):
+                if y.find(st.DELIMIT) < 0:
+                    ss.add(y)
+            #print 'pincer', list(st.pincer(s, x.pos, x.minimal)), x
+        self.assertEqual(sorted(set(all_substrings(s[0:len(s)-1]))), sorted(ss))
+
+if __name__ == '__main__':
+    unittest.main()
